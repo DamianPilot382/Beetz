191034103d0ae4069c46ebf5685ba9c5bdcfc408
BoredDoodler <kriscanflyinsky@gmail.com>
2016-04-22 09:21:25 -0700

    Add colors file add color changer

--- mobile/src/main/java/com/pilotcraftsystems/main/MainActivity.java
+++ mobile/src/main/java/com/pilotcraftsystems/main/MainActivity.java
@@ -1,8 +1,23 @@
 package com.pilotcraftsystems.main;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.Wearable;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
 
 
---
+++ wear/src/main/java/com/pilotcraftsystems/games/ColorChanger.java
@@ -0,0 +1,8 @@
+package com.pilotcraftsystems.games;
+
+/**
+ * Created by csastudent2015 on 4/22/16.
+ * csastudent2015 is super cool.
+ */
+public class ColorChanger {
+}
