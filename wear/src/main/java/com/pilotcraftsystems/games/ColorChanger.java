ac095bb91b95394c445496d6e4a9528f412a10c5
BoredDoodler <kriscanflyinsky@gmail.com>
2016-04-26 08:50:41 -0700

    Do Color math *Untested*

--- wear/src/main/java/com/pilotcraftsystems/games/ColorChanger.java
+++ wear/src/main/java/com/pilotcraftsystems/games/ColorChanger.java
@@ -5,4 +5,35 @@ package com.pilotcraftsystems.games;
  * csastudent2015 is super cool.
  */
 public class ColorChanger {
+    private int max;
+    private int min;
+    private int target;
+    public final int MAX_RGB=255;
+    public final int MIN_RGB=0;
+    private static int red;
+    private static int blue;
+    public ColorChanger(int max, int min, int target){
+    max=this.max;
+        min=this.min;
+        target=this.target;
+        red=MAX_RGB;
+        blue=MAX_RGB;
+
+    }
+
+    public void updateRGB(int heartBeet){
+        int range;
+        if(heartBeet==target){
+            red=255;
+            blue=255;
+        }else if(heartBeet<target){
+            range=target-min;
+            red= (int)(((heartBeet-min)/range)*MAX_RGB);
+            blue=255;
+        }else{
+            range=max-target;
+            blue=(int)(((heartBeet-target)/range)*MAX_RGB);
+        }
+    }
+
 }
